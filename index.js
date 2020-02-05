fbc6bc34bf3b25f608bb1641d66db1a5c2c9cfd9
Iskender <jiskender1@gmail.com>
2020-02-05 13:36:04 +0600

    added function

---
+++ index.js
@@ -0,0 +1,11 @@
+let menu= document.querySelector("button");
+let aside =document.querySelector("aside");
+
+menu.addEventListener('click',function(){
+    if(aside.style.left == "-200px"){
+               aside.style.left = "0px";
+    }
+   else{aside.style.left = "-200px";
+        
+   }
+});
