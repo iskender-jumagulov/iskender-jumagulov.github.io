581e507f851d9cccf0d144adc4614f18378472be
Iskender <jiskender1@gmail.com>
2020-02-17 13:09:37 +0600

    added photo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let menu= document.querySelector("button");
 let aside =document.querySelector("aside");
 
 menu.addEventListener('click',function(){
-    if(aside.style.left == "-200px"){
+    if(aside.style.left == "200px"){
                aside.style.left = "0px";
     }
    else{aside.style.left = "-200px";
